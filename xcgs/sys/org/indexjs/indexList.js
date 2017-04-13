7a7806422eb24d0887cf9629befdccd47387e4e1
litisn <373974360@qq.com>
2017-04-13 13:41:02 +0800

    删除目录xcgs

--- xcgs/sys/org/indexjs/indexList.js
+++
@@ -1,6 +0,0 @@
-var UserLogin = jsonrpc.UserLoginRPC;
-var MenuBean = new Bean("com.deya.wcm.bean.org.operate.MenuBean",true);	
-
-var DeptRPC = jsonrpc.DeptRPC;
-var DeptBean = new Bean("com.deya.wcm.bean.org.dept.DeptBean",true);
-
