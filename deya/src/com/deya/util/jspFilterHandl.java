01e811f17f002ebda833487a824dd0ee532b899a
like <373974360@qq.com>
2017-08-24 14:33:00 +0800

    修改URL存在宽字节跨站漏洞 修改目标服务器启用了TRACE方法

--- deya/src/com/deya/util/jspFilterHandl.java
+++ deya/src/com/deya/util/jspFilterHandl.java
@@ -8,7 +8,7 @@ import java.net.URLDecoder;
 import java.util.Enumeration;
 
 public class jspFilterHandl {
-    private static String[] filter_str = {"%22", "%27", "%3E", "%3e", "%3C", "%3c", "/*", "\\", "union", "--", "1=1", "and ", "concat", "acustart", "application", "script", "location", "limit ", "alert", "iframe", "set-cookie", "+", "or ", "drop table", "asc\\(", "mid\\(", "char\\(", "net user", "exists", "alter",
+    private static String[] filter_str = {"%df","%5c","%27","%20","%22", "%27", "%3E", "%3e", "%3C", "%3c", "/*", "\\", "union", "--", "1=1", "and ", "concat", "acustart", "application", "script", "location", "limit ", "alert", "iframe", "set-cookie", "+", "or ", "drop table", "asc\\(", "mid\\(", "char\\(", "net user", "exists", "alter",
             "+acu+", "onmouseover", "header", "exec ", "insert ", "select ", "delete ", "trancate", "update ", "updatexml", "extractvalue", "href=", "data:text", "declare", "master", "execute", "xp_cmdshell", "netlocalgroup", "count\\(", "restore", "floor", "ExtractValue", "UpdateXml",
             "injected", "ACUstart", "ACUend", "():;", "acu:Expre", "window.location.href", "document", "parameter: ", "<OBJECT", "javascript", "confirm", "<script>", "</script>", "..", "<img>", "</img>", "cat ", "click", "function", "prompt(", "<a", ";", "'", "“", "”", "‘", "’"};
     private static String no_filter_jsp;
