cc8907b4b094b9c2ece68c4c6db8346882a2158d
like <373974360@qq.com>
2018-05-11 11:56:54 +0800

    删除crimon这个jar包可以解决跟银海单点登录报错的问题

--- deya/src/main/java/com/deya/util/xml/XmlManager.java
+++ deya/src/main/java/com/deya/util/xml/XmlManager.java
@@ -9,24 +9,23 @@
 
 package com.deya.util.xml;
 
-import java.util.Properties;
-import java.util.Vector;
-import java.io.*;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl;
+import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.xml.sax.InputSource;
-import javax.xml.parsers.*;
 import org.w3c.dom.traversal.NodeIterator;
-import org.apache.xpath.XPathAPI;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.Properties;
+import java.util.Vector;
 
 
 /**
@@ -44,7 +43,7 @@ public class XmlManager {
 	// Remove by sman on 2003-9-3
 
 	static {
-		dbf = new org.apache.crimson.jaxp.DocumentBuilderFactoryImpl();
+		dbf = new DocumentBuilderFactoryImpl();
 		dbf.setCoalescing(true);
 		dbf.setIgnoringElementContentWhitespace(true);
 		dbf.setIgnoringComments(true);
