72595ee59238c23ecd427b8786da43045f5f984a
chaoweima <404827131@qq.com>
2018-12-18 09:36:35 +0800

    base64转码解码问题

--- deya/src/com/deya/util/Base64Utiles.java
+++ deya/src/com/deya/util/Base64Utiles.java
@@ -1,13 +1,8 @@
 package com.deya.util;
 
 import java.io.*;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import org.apache.commons.codec.binary.Base64;
 
 /**
  * base64 与 file 之间的相互转化
@@ -25,7 +20,8 @@ public class Base64Utiles {
             byte[] buffer = new byte[(int) file.length()];
             inputFile.read(buffer);
             inputFile.close();
-            return gzip(new BASE64Encoder().encode(buffer))+ext;
+            String bTemp = Base64.encodeBase64String(buffer)+ext;
+            return bTemp;
         } catch (Exception e) {
             throw new RuntimeException("文件路径无效\n" + e.getMessage());
         }
@@ -33,10 +29,9 @@ public class Base64Utiles {
 
     // 将 base64 转化为 file
     public static boolean base64ToFile(String base64, String path) {
-        base64 = gunzip(base64);
         byte[] buffer;
         try {
-            buffer = new BASE64Decoder().decodeBuffer(base64);
+            buffer = Base64.decodeBase64(base64);
             FileOutputStream out = new FileOutputStream(path);
             out.write(buffer);
             out.close();
@@ -46,79 +41,9 @@ public class Base64Utiles {
         }
     }
 
-    public static String gzip(String primStr) {
-        if (primStr == null || primStr.length() == 0) {
-            return primStr;
-        }
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        GZIPOutputStream gzip = null;
-        try {
-            gzip = new GZIPOutputStream(out);
-            gzip.write(primStr.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (gzip != null) {
-                try {
-                    gzip.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return new sun.misc.BASE64Encoder().encode(out.toByteArray());
-    }
-
-    public static String gunzip(String compressedStr) {
-        if (compressedStr == null) {
-            return null;
-        }
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ByteArrayInputStream in = null;
-        GZIPInputStream ginzip = null;
-        byte[] compressed = null;
-        String decompressed = null;
-        try {
-            compressed = new sun.misc.BASE64Decoder().decodeBuffer(compressedStr);
-            in = new ByteArrayInputStream(compressed);
-            ginzip = new GZIPInputStream(in);
-
-            byte[] buffer = new byte[1024];
-            int offset = -1;
-            while ((offset = ginzip.read(buffer)) != -1) {
-                out.write(buffer, 0, offset);
-            }
-            decompressed = out.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (ginzip != null) {
-                try {
-                    ginzip.close();
-                } catch (IOException e) {
-                }
-            }
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                }
-            }
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-
-        return decompressed;
-    }
-
     public static void main(String[] args) {
-        String str = fileToBase64("/Users/chaoweima/Downloads/2018版-信息协同接口设计-外部协作的信息协同接口1.0.doc");
+        String str = fileToBase64("/Users/chaoweima/Downloads/OA接口说明文档.docx");
+        base64ToFile(str, "/Users/chaoweima/Downloads/111.docx");
         System.out.println(str);
     }
 }
